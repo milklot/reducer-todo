cadbc980760a8ff411bd8f687ef83f26858b0bb7
milklot <vbiostflex@gmail.com>
2020-12-16 19:54:07 -0500

    created react todo app

---
+++ todo/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React from "react";
+
+const todoArr = [
+  {
+    task: "first thing todo",
+    id: 11,
+    completed: false
+  },
+  {
+    task: "second thing todo",
+    id: 11,
+    completed: false
+  }
+];
+
+
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
