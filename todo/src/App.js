9536c8e7d17a7db234872a4c11ddaef334b7a33e
milklot <vbiostflex@gmail.com>
2020-12-16 21:35:50 -0500

    mvp

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,18 @@
 import './App.css';
-import React from "react";
-
-const todoArr = [
-  {
-    task: "first thing todo",
-    id: 11,
-    completed: false
-  },
-  {
-    task: "second thing todo",
-    id: 11,
-    completed: false
-  }
-];
-
-
+import React, { useReducer } from 'react';
+import { initialState, reducer } from './reducers/reducer';
+import ToDoForm from './components/todoForm';
+import ToDoList from './components/todoList';
 
 function App() {
-  return (
-    <div>
 
+  const [state, dispatch] = useReducer (reducer, initialState);
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <ToDoForm dispatch={dispatch}/>
+      <ToDoList state={state} dispatch={dispatch}/>
     </div>
   );
 }
